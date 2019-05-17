b2a4a9bdeff039771a71088453ac55035de7ba31
d-jok <31610302+d-jok@users.noreply.github.com>
2019-05-17 12:39:12 +0300

    Laba3

---
+++ Laba3/StudentCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laba3
+{
+    class StudentCollection
+    {
+        public List<Student> StudentInfo = new List<Student>();
+
+        public void AddDefaults(int n)
+        {
+            StudentInfo.Add(TestCollections.GenerateInfo(n));
+        }
+
+        public void AddStudents(params Student[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                StudentInfo.Add(input[i]);
+            }
+        }
+
+        public void SortSurname()
+        {
+            StudentInfo.Sort();// (a, b) => a.CompareTo(b)); //need to think
+        }
+
+        public void SortDate()
+        {
+            StudentInfo.Sort((a, b) => a.Compare(a, b));    //need to think
+        }
+
+        public void SortMark()
+        {
+            Student.Support U = new Student.Support();
+            StudentInfo.Sort((a, b) => U.Compare(a, b));    //need to think
+        }
+
+        public double MaxMark  //Find Max MediumMark in List
+        {
+            get
+            {
+                return StudentInfo.Max(a => a.MediumMark);
+            }
+        }
+
+        public IEnumerable<Student> EducationForm()
+        {
+            return StudentInfo.Where(x => x.FormInfo == Person.Education.Master);
+        }
+
+        public List<Student> AverageMarkGroup(double value)
+        {
+            return StudentInfo.GroupBy(p => p.MediumMark == 4.5).Where(p => p.Key).First().ToList();
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            foreach (Student v in StudentInfo)
+                Console.WriteLine(v + Environment.NewLine);
+            return "";
+        }
+
+        public string ToShortString()
+        {
+            foreach (Student v in StudentInfo)
+                Console.WriteLine(v.ToShortString() + Environment.NewLine);
+            return "";
+        }
+    }
+}
---
+++ Laba3/main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Laba3
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            StudentCollection S = new StudentCollection();
+            //TestCollections T = new TestCollections(3);
+
+            for(int i = 0; i < 5; i++)
+                S.AddDefaults(i);
+                S.ToString();
+
+            //SORT
+            //S.SortSurname();
+            //S.ToString();
+
+            //S.SortDate();
+            //S.ToString();
+
+            //S.SortMark();
+            //S.ToString();
+
+            //OPERATION
+            //Console.WriteLine(S.MaxMark);
+            //S.EducationForm();
+            //S.ToString();
+            //S.AverageMarkGroup(4.5);
+            //S.ToString();
+
+            //TEST COLLECTION
+            //Dictionary<string, int> J;
+            //J = T.GetTime(S.StudentInfo[3]);
+
+            //foreach(KeyValuePair<string, int> v in J)
+            //{
+            //    Console.WriteLine("{0}, \tTime is {1}", v.Key, v.Value);
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
